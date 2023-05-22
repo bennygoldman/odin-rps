82862d989709fc386322f2b1e2022a15df61462f
bennygoldman <goldmanb@gmail.com>
2023-05-22 11:37:57 -0400

    initial commit with first batch of pseudocode

---
+++ script.js
@@ -0,0 +1,25 @@
+// function getComputerChoice() {
+// rondomly
+// returns 'rock', 'paper', or 'scissors'
+// }
+// to make sure this is returning the expected output, 
+// console.log(getComputerChoice)
+
+// function playRound(playerSelection, computerSelection) {
+// return winner+outcome in the format "You Lose! Paper beats Rock"
+// }
+
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+// function game() {
+// Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+// }
+
+// other stuff 
+
+// function getPlayerChoice() {
+// let playerSelection = prompt('choose rock, paper, or scissors')
+// }
