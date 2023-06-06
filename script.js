3309fb71e4aa218cd91802c4a93aedbe888ec0db
bennygoldman <goldmanb@gmail.com>
2023-06-06 14:05:44 -0400

    complete logic - move game from console to dom
    would still like to style and refactor so score appears above reset button when final score is reached.

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 let playerScore = 0;
 let computerScore = 0;
-const winningScore = 3;
-const buttons = document.querySelectorAll('button');
+const winningScore = 5;
+const container = document.querySelector('.results');
+const resultPara = document.createElement('p');
+const currentScorePara = document.createElement('p');
+const finalScorePara = document.createElement('p');
+const resetButton = document.createElement('button')
+resetButton.textContent = "Reset";
+resetButton.addEventListener('click', resetGame);
+const choices = document.querySelectorAll('.choice');
 
-buttons.forEach(button => button.addEventListener('click', playRound))
-// function game() {
+initalizeGame();
 
-//     while (playerScore < winningScore && computerScore < winningScore) {
+function initalizeGame() {
+    choices.forEach(choice => choice.addEventListener('click', game))
+}
 
-//         const playerSelection = getPlayerChoice();
-//         const computerSelection = getComputerChoice();
-//         playRound(playerSelection, computerSelection);
-//         if (playerScore === winningScore) {
-//             console.log('GAME OVER! YOU WIN! FINAL SCORE:');
-//         } else if (computerScore === winningScore) {
-//             console.log('GAME OVER! YOU LOSE! FINAL SCORE:');
-//         }
-//         displayCurrentScore();
-// }
+function game() {
+    const computerSelection = getComputerChoice();
+    const playerSelection = this.id;
+    if (playerScore < winningScore && computerScore < winningScore) {
+        playRound(playerSelection, computerSelection);
+    };
+    if (playerScore === winningScore || computerScore === winningScore) {
+        endGame();
+    };
+    displayCurrentScore();
+}
 
-// }
 function getComputerChoice() {
     let randomNumber = (Math.floor(Math.random() * 3))
     switch (randomNumber) {
@@ -32,42 +40,57 @@ function getComputerChoice() {
     }
 };
 
-// function getPlayerChoice() {
-//     let choice = prompt('choose rock, paper, or scissors')
-//     const lowercaseChoice = choice.toLowerCase();
-//     if (lowercaseChoice === 'rock' || lowercaseChoice === 'paper' || lowercaseChoice === 'scissors') {
-//         return lowercaseChoice;
-//     } else {
-//         console.log('Invalid choice. Please try again.');
-//         return getPlayerChoice();
-//     }
-// }
-
-// function playRound(playerSelection, computerSelection) {
-function playRound() {
-    const computerSelection = getComputerChoice();
-    const playerSelection = this.classList.value;
+function playRound(playerSelection, computerSelection) {
     const playerSelectionFormatted = (playerSelection.charAt(0).toUpperCase()) + playerSelection.slice(1)
     const computerSelectionFormatted = (computerSelection.charAt(0).toUpperCase()) + computerSelection.slice(1)
 
     if (playerSelection === computerSelection) {
-        console.log("It's a Tie! Try again");
+        resultPara.textContent = "It's a Tie! Try again";
     } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
-        console.log(`You Win! ${playerSelectionFormatted} beats ${computerSelectionFormatted}`);
+        resultPara.textContent = `You Win! ${playerSelectionFormatted} beats ${computerSelectionFormatted}`;
         playerScore += 1;
     } else {
-        console.log(`You Lose! ${computerSelectionFormatted} beats ${playerSelectionFormatted}`);
+        resultPara.textContent = `You Lose! ${computerSelectionFormatted} beats ${playerSelectionFormatted}`
         computerScore += 1;
     }
+    container.appendChild(resultPara);
 };
 
 function displayCurrentScore() {
     const currentScore = `MAN: ${playerScore} ... MACHINE ${computerScore}`;
-    console.log(currentScore);
+    currentScorePara.textContent = currentScore;
+    container.appendChild(currentScorePara);
 }
 
-// game();
+function endGame() {
+    choices.forEach(choice => choice.removeEventListener('click', game));
+
+    if (playerScore === winningScore) {
+        finalScorePara.textContent = 'GAME OVER! YOU WIN! FINAL SCORE:';
+        finalScorePara.style.color = 'green';
+    } else if (computerScore === winningScore) {
+        finalScorePara.textContent = 'GAME OVER! YOU LOSE! FINAL SCORE:';
+        finalScorePara.style.color = 'red';
+    }
+    container.appendChild(finalScorePara);
+    container.appendChild(resetButton);
+
+}
+
+function resetGame() {
+    initalizeGame();
+    playerScore = 0;
+    computerScore = 0;
+    resultPara.textContent = '';
+    currentScorePara.textContent = '';
+    finalScorePara.textContent = '';
+    container.removeChild(resetButton);
+
+}
+
+
+
